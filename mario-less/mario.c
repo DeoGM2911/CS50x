1f32cca819bf6821fe052126891d815c34f799df
DeoGM2911 <dung20042911@gmail.com>
2023-02-01 21:49:16 +0700

    modified:   mario-less/mario.c

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+void build(int h, string c);
+
+int main(void)
 {
-    
+    // Get the height
+    int n;
+    do
+    {
+    n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+
+    // Build the pyramid
+    string c1 = "#";
+    for (int i = 1; i <= n; i++)
+    {
+        build(i, c1);
+    }
+}
+
+void build(int h, string s)
+{
+    for (int k = 1; k <= h; k++)
+    {
+        printf("%s", s);
+    }
+    printf("\n");
 }
