9dec7ae6302aeec5e7a07381e5f370e5316ecf3a
DeoGM2911 <dung20042911@gmail.com>
2023-02-03 10:15:19 +0700

    final version credit.c 	modified:   credit/credit.c

--- credit/credit.c
+++ credit/credit.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-    // *Get the credit card number
+    // Get the credit card number
     long cred_num;
     do
     {
@@ -12,7 +12,7 @@ int main(void)
     }
     while (cred_num < 0);
 
-    // *Count the number of digits
+    // Count the number of digits
     int count = 1;
     long limit = 10;
     while (limit <= cred_num)
@@ -20,61 +20,63 @@ int main(void)
         count++;
         limit = (long) pow(10, count);
     }
+
     /*
-    *Calculate the sum to check the Luhn's Algorithm
+    Calculate the sum to check the Luhn's Algorithm
     The placeholder is the value of the ith digit from the left
     */
     int sum = 0;
     int plholder;
     int frt_2_digit = 0;
-    for (int i = count; i > 0; i--)
+    for (int i = count - 1; i >= 0; i--)
     {
-        if (i % 2 != 0)
+        // Calculate the sum
+        if ((i + 1) % 2 != 0)
         {
-            plholder = (int)(cred_num / ((long) pow( 10, i)));
+            plholder = (int)(cred_num / ((long) pow(10, i)));
             sum += plholder;
         }
         else
         {
-            plholder = (int)(cred_num / ((long) pow( 10, i)));
-            sum += 2 * plholder;
+            plholder = (int)(cred_num / ((long) pow(10, i)));
+            sum += (int)((2 * plholder) / 10) + ((2 * plholder) % 10);
         }
         // Get the first two digits
-        if (i == count)
+        if (i == count - 1)
         {
             frt_2_digit += 10 * plholder;
         }
-        if (i == count - 1)
+        if (i == count - 2)
         {
             frt_2_digit += plholder;
         }
         // Change the cred_num for the next loop to calculate the place holder
-        cred_num -= (long)(plholder * ((long) pow( 10, i)));
+        cred_num -= (long)(plholder * ((long) pow(10, i)));
     }
-    // *Check whether a credit card number is valid 
+    // Check whether a credit card number is valid
     if (sum % 10 != 0)
     {
         printf("INVALID\n");
     }
+    // Check whether a card is Mastercard, American Express or Visa
     else
     {
         if ((frt_2_digit == 34 || frt_2_digit == 37) && (count == 15))
         {
-            printf("AMERICAN EXPRESS\n");
+            printf("AMEX\n");
         }
-        else if ((frt_2_digit == 51 || frt_2_digit == 52 || 
-        frt_2_digit == 53 || frt_2_digit == 54 || frt_2_digit == 55) && (count == 16))
+        else if (((frt_2_digit >= 51) && (frt_2_digit <= 55)) && (count == 16))
         {
             printf("MASTERCARD\n");
         }
-        else if ((frt_2_digit % 10 == 4) && (count == 16 || count == 13))
+        else if ((count == 16 || count == 13) && ((int)(frt_2_digit / 10) == 4))
         {
             printf("VISA\n");
         }
         else
         {
-            printf("Not a supported provider!\n");
+            printf("INVALID\n");
         }
-        
+        // Other type of credit card number would not be accepted
     }
 }
