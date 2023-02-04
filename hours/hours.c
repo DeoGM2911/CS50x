2d8555b3b610afcfc9dbf2e2ac1a2e38f87529b8
DeoGM2911 <dung20042911@gmail.com>
2023-02-04 15:22:12 +0700

    Add hours.c in week 2 	modified:   hours/hours.c

--- hours/hours.c
+++ hours/hours.c
@@ -8,24 +8,39 @@ int main(void)
 {
     int weeks = get_int("Number of weeks taking CS50: ");
     int hours[weeks];
-
+    // Take the user's inputs 
     for (int i = 0; i < weeks; i++)
     {
         hours[i] = get_int("Week %i HW Hours: ", i);
     }
-
+    // Prompt the user for the choice
     char output;
     do
     {
         output = toupper(get_char("Enter T for total hours, A for average hours per week: "));
     }
     while (output != 'T' && output != 'A');
-
+    // Print out the desired output
     printf("%.1f hours\n", calc_hours(hours, weeks, output));
 }
 
-// TODO: complete the calc_hours function
 float calc_hours(int hours[], int weeks, char output)
 {
-
+    float total = 0;
+    float average;
+    // Calculate the total
+    for (int i = 0; i < weeks; i++)
+    {
+        total += hours[i];
+    }
+    // Calculate the average
+    average = total / (float) weeks;
+    if (output == 'T')
+    {
+        return total;
+    }
+    else
+    {
+        return average;
+    }
 }
