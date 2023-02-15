492cc7981c9c66bc31385ba469da69c3e49b042c
DeoGM2911 <dung20042911@gmail.com>
2023-02-15 12:22:31 +0700

    modified:   week3/pset3/plurality/plurality.c

--- week3/pset3/plurality/plurality.c
+++ week3/pset3/plurality/plurality.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 
 // Max number of candidates
 #define MAX 9
@@ -50,8 +51,8 @@ int main(int argc, string argv[])
     // Loop over all voters
     for (int i = 0; i < voter_count; i++)
     {
+        // Get the user's choice
         string name = get_string("Vote: ");
-
         // Check for invalid vote
         if (!vote(name))
         {
@@ -66,13 +67,56 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    // Linear search as the candidate list is not sorted
+    for (int i = 0; i < MAX; i++)
+    {
+        // Stop the iteration at the end of the list
+        if (strcasecmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+        if (candidates[i + 1].name == NULL)
+        {
+            break;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    // Find the person (people) that has the highest count
+    int largest;
+    for (int i = 0; i < MAX; i++)
+    {
+        // Set default value for largest
+        if (i == 0)
+        {
+            largest = candidates[0].votes;
+        }
+        if (largest < candidates[i].votes)
+        {
+            largest = candidates[i].votes;
+        }
+        // Stop the iteration at the end of the list
+        if (candidates[i + 1].name == NULL)
+        {
+            break;
+        }
+    }
+    for (int i = 0; i < MAX; i++)
+    {
+        if (candidates[i].votes == largest)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+        // Stop the iteration at the end of the list
+        if (candidates[i + 1].name == NULL)
+        {
+            break;
+        }
+    }
     return;
 }
