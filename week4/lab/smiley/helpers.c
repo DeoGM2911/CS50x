40c7e0bf29fe816f816db0c2a4912718bd513b71
DeoGM2911 <dung20042911@gmail.com>
2023-02-21 10:09:35 +0700

    COmplete lab in week4 	modified:   week4/lab/smiley/helpers.c

--- week4/lab/smiley/helpers.c
+++ week4/lab/smiley/helpers.c
@@ -3,4 +3,22 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    // Black pixel == 0x00;
+    // White pixel == 0xff;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtBlue == 0x00
+            && image[i][j].rgbtRed == 0x00
+            && image[i][j].rgbtGreen == 0x00)
+            {
+                image[i][j].rgbtRed == 0xe0;
+                image[i][j].rgbtGreen == 0x6c;
+                image[i][j].rgbtBlue == 0x75;
+            }
+        }
+        
+    }
+    
 }
